6b2a8d3e3b211005f507e3794577c35c92b12750
ByeongJu0411 <wjsqudwn981789@gmail.com>
2026-07-10 15:21:27 +0900

    chore: cors 설정 추가 및 요청 포트 전부 열기 (#8)

--- mocks/dev-server.ts
+++ mocks/dev-server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { createMiddleware } from '@mswjs/http-middleware';
 import { handlers } from './handlers';
 
 const app = express();
 const PORT = 9090;
 
+app.use(cors());
 app.use(express.json());
 app.use(createMiddleware(...handlers));
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-
+import './globals.css';
 export const metadata: Metadata = {
   title: 'ZZAZO',
   description: 'ZZAZO 웹 서비스',
